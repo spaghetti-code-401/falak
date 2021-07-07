11b7452d75535ea876d95e9f0931e11e74d40397
qaisw96 <qaisw14@gmail.com>
2021-07-07 10:53:01 +0300

    setup02

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="headerWrapper">
+                
+                {/* <div className="headerLogo"></div> */}
+            </div>
+        </header>
+    )
+}
+
+export default Header;
