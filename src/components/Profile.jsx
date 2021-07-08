072a29d15e31e1a9cd88384f606726531d185e8f
qaisw96 <qaisw14@gmail.com>
2021-07-08 11:38:01 +0300

    done

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+
+
+const Profile = (props) => {
+
+    return (
+        <section className="profile">
+            <div className="profileWrapper">
+                <img src="https://tipsmake.com/data1/thumbs/how-to-extract-img-files-in-windows-10-thumb-bzxI4IDgg.jpg" alt="" />
+                
+            </div>
+
+        </section>
+    )
+}
+
+export default Profile 
