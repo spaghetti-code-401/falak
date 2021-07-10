1c902f42f688563959915cace42f26f955b69ff9
AliBakerSartawi <alisartawi@live.com>
2021-07-10 17:34:08 +0300

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import './header.scss'
+import React from 'react';
+import './header.scss';
 import HomeIcon from '@material-ui/icons/Home';
 import TextsmsIcon from '@material-ui/icons/Textsms';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export const Header = () => {
-    return (
-        <header className="header glass">
-            <div className="headerWrapper">
-                <div className="headerLogo" >Social</div>
-                <div className="headerSearch" >
-                    <input type="text" placeholder="search" className="searchHeaderInput" />
-                </div>
-                <div className="headerIcons" >
-                    <HomeIcon className="homeIcon"/>
-                    <TextsmsIcon className="messageIcon"/>
-                    <p className="logOut">Logout</p>
-                    <AccountCircleIcon className="avatarIcon"/>
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="header glass">
+      <div className="headerLogo">social</div>
+      <div className="headerSearch">
+        <input type="text" placeholder="search" className="searchHeaderInput glass2" />
+      </div>
+      <div className="headerIcons">
+        <HomeIcon className="homeIcon" />
+        <TextsmsIcon className="messageIcon" />
+        <p className="logOut">Logout</p>
+        {/* <AccountCircleIcon className="avatarIcon" /> */}
+        <img src="/images/test.jpg" alt="" className="headerUserImage" />
+      </div>
+    </header>
+  );
+};
 
-  
 export default Header;
