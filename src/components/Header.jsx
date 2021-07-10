1ae9b206d0ffe3f10d70d48d98b67e863a9b5317
AliBakerSartawi <alisartawi@live.com>
2021-07-10 20:53:40 +0300

    search animation eshi 6age3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import './header.scss';
 import HomeIcon from '@material-ui/icons/Home';
 import TextsmsIcon from '@material-ui/icons/Textsms';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { SearchOutlined } from '@material-ui/icons';
 
 export const Header = () => {
   return (
     <header className="header glass">
       <div className="headerLogo">social</div>
-      <div className="headerSearch">
-        <input type="text" placeholder="search" className="searchHeaderInput glass2" />
+      <div className="headerSearch glass2">
+        <SearchOutlined className="headerSearchIcon"/>
+        <input type="text" placeholder="search" className="headerSearchInput" />
       </div>
       <div className="headerIcons">
         <HomeIcon className="homeIcon" />
