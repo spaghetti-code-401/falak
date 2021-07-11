898348fbdcb0a298eb4ef5abd3c74300bd5a56f0
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-07-11 12:14:45 +0300

    sign in sign up style

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,34 +5,6 @@ import { Button,TextField  } from '@material-ui/core'
 export default function SignUp() {
     return (
 
-        <div className="signIn">
-            <div className='signInWrapper'>
-
-                <div className="signInLeft">
-                    <h1>
-                        Lorem ipsum
-                    </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nostrum facere quasi ipsum culpa quae pariatur quia recusandae omnis
-                    </p>
-                </div>
-                <div className="signInRight">
-                    <form className="form">
-                        <TextField label="Username" variant="outlined" size="small" className="input" type="text"   />
-                        <TextField label="Email" variant="outlined" size="small" className="input" type="text"   />
-                        <TextField label="Password" variant="outlined" size="small" className="input" type="text"   />
-                        <TextField label="Password Again" variant="outlined" size="small" className="input" type="password"   />
-                        <Button variant="contained" color="primary" className='button'>
-                            Sign Up
-                        </Button>
-                        <p>Already have an account?</p>
-                        <Button variant="contained" color="secondary" className='newAccountButton' >
-                            log In
-                        </Button>
-                    </form>
-                </div>
-            </div>
-        </div>
         // <div className="signIn">
         //     <div className='signInWrapper'>
 
@@ -46,21 +18,49 @@ export default function SignUp() {
         //         </div>
         //         <div className="signInRight">
         //             <form className="form">
-        //                 <input placeholder="Username"  className="input" type="text"   />
-        //                 <input placeholder="Email"  className="input" type="text"   />
-        //                 <input placeholder="Password"  className="input" type="text"   />
-        //                 <input placeholder="Password Again"  className="input" type="password"   />
-        //                 <button  className='button'>
+        //                 <TextField label="Username" variant="outlined" size="small" className="input" type="text"   />
+        //                 <TextField label="Email" variant="outlined" size="small" className="input" type="text"   />
+        //                 <TextField label="Password" variant="outlined" size="small" className="input" type="text"   />
+        //                 <TextField label="Password Again" variant="outlined" size="small" className="input" type="password"   />
+        //                 <Button variant="contained" color="primary" className='button'>
         //                     Sign Up
-        //                 </button>
+        //                 </Button>
         //                 <p>Already have an account?</p>
-        //                 <button  className='newAccountButton' >
+        //                 <Button variant="contained" color="secondary" className='newAccountButton' >
         //                     log In
-        //                 </button>
+        //                 </Button>
         //             </form>
         //         </div>
         //     </div>
         // </div>
-         
+        <div className="signIn">
+            <div className='signInWrapper glass'>
+
+                <div className="signInLeft">
+                    <h1>
+                        Lorem ipsum
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nostrum facere quasi ipsum culpa quae pariatur quia recusandae omnis
+                    </p>
+                </div>
+                <div className="signInRight glass2">
+                    <form className="form">
+                        <input placeholder="Username"  className="input glass2" type="text"   />
+                        <input placeholder="Email"  className="input glass2" type="text"   />
+                        <input placeholder="Password"  className="input glass2" type="password"   />
+                        <input placeholder="Password Again"  className="input glass2" type="password"   />
+                        <hr className='hr' />
+                        <button  className='button glass2 signUpButton'>
+                            Sign Up
+                        </button>
+                        <p>Already have an account?</p>
+                        <button  className='newAccountButton glass2' >
+                            log In
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>         
     )
 }
