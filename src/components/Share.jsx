f7ed84f6605ad1beaf5a39c0521b458fd8e7de3e
AliBakerSartawi <alisartawi@live.com>
2021-07-11 18:36:55 +0300

    share done

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -7,31 +7,103 @@ import Button from '@material-ui/core/Button';
 import ImageIcon from '@material-ui/icons/Image';
 import { useAuth } from '../context/AuthContext';
 import usePF from '../hooks/usePF';
+import { useRef, useState } from 'react';
+import axios from 'axios';
+import { Cancel } from '@material-ui/icons';
 
 const Share = (props) => {
-  const {user} = useAuth()
-  const {glass2} = useTheme()
-  const PF = usePF()
+  const { user } = useAuth();
+  const { glass2, lightText } = useTheme();
+  const PF = usePF();
+  const postDesc = useRef();
+  const [file, setFile] = useState(null);
+
+  console.log(file);
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: postDesc.current.value
+    };
+
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append('file', file);
+      data.append('name', fileName);
+
+      newPost.img = fileName;
+    }
+
+    try {
+      await axios.post(
+        'https://api-social-mern.herokuapp.com/api/posts',
+        newPost
+      );
+      // instead of reloading window, update post state
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <section className="share">
       <div className={`shareWrapper ${glass2}`}>
+        <div className="shareHeader">
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + 'person/noAvatar.png'
+            }
+            alt=""
+            className="shareImg"
+          />
+          <input
+            placeholder={`What's on your mind ${user.username}?`}
+            className={`shareInput ${lightText}`}
+            ref={postDesc}
+          />
 
-          <div className="shareHeader">
-              <img src={PF+user.profilePicture} alt="" className="shareImg" />
-              <form noValidate autoComplete="off">
-                <textarea placeholder={"What's on your mind "+user.username}  className={`shareTextField ${glass2}`} id="" cols="56.5" rows="57"></textarea>
-              </form>
+          {/* <form noValidate autoComplete="off">
+            <textarea
+              placeholder={"What's on your mind " + user.username}
+              className={`shareTextField ${glass2}`}
+              id=""
+              cols="56.5"
+              rows="57"></textarea>
+          </form> */}
+        </div>
+
+        {file && (
+          <div className="shareFileContainer">
+            <img src={URL.createObjectURL(file)} alt="" className="shareFile" />
+            <Cancel className="shareCancelImg" onClick={() => setFile(null)} />
           </div>
+        )}
 
-          <div className="shareBottom">
-             
-              <label htmlFor="file-upload" className="shareImageLabel"> <ImageIcon  type="file" className="shareImageIcon"/>choose img</label>
-              <input type="file"  id="file-upload"/>
-              <Button className="btnPost glass2" variant="contained" color="default">
-                Post
-              </Button>
-          </div>
-
+        <form className="shareBottom" onSubmit={submitHandler}>
+          <label htmlFor="file-upload" className={`shareImageLabel ${lightText}`}>
+            <ImageIcon type="file" className={`shareImageIcon ${lightText}`} />
+            Choose file
+            <input
+              style={{ display: 'none' }}
+              type="file"
+              id="file-upload"
+              accept=".png,.jpeg,.jpg,.gif"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </label>
+          <Button
+            type="submit"
+            className={`btnPost ${glass2} ${lightText}`}
+            variant="contained"
+            style={{borderRadius: '10px'}}
+            color="default">
+            Share
+          </Button>
+        </form>
       </div>
     </section>
   );
