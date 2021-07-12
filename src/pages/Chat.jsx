99aa3e638b1e3e4a592cc63610b4832f8ec8e24d
AliBakerSartawi <alisartawi@live.com>
2021-07-12 11:11:37 +0300

    socket connection

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Header from '../components/Header';
 import ChatSideBar from '../components/ChatSideBar';
 import Messages from '../components/Messages';
 import './chat.scss';
+import {io} from 'socket.io-client'
 
 export default function Chat() {
+  const socket = useRef()
+
+  useEffect(() => {
+    socket.current = io.connect('ws://localhost:8900/')
+  }, [])
+  
   return (
     <div>
       <Header />
