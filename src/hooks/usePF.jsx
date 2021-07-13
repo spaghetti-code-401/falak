744379acb3189609d2e317dc338c759327234a5a
AliBakerSartawi <alisartawi@live.com>
2021-07-13 13:16:51 +0300

    dynamic PF

--- src/hooks/usePF.jsx
+++ src/hooks/usePF.jsx
@@ -1,7 +1,13 @@
 const usePF = () => {
   // const PF = 'https://falak-api.herokuapp.com/images/'
-  const PF = 'http://localhost:3000/images/'
+  // const PF = 'http://localhost:3000/images/'
+  let PF;
+  if (window.location.href[4] === ':') {
+    PF = 'http://localhost:3000/images/';
+  } else {
+    PF = 'https://falak-social.netlify.app/images/';
+  }
   return PF;
-}
+};
 
-export default usePF
+export default usePF;
